56ca1f0f22f6e4cdbf5ca6db5ae0036478d77ee5
jltc08 <97373492+jltc08@users.noreply.github.com>
2022-10-31 12:31:04 -0400

    Add files via upload

---
+++ pumpkincontest2021.py
@@ -0,0 +1,29 @@
+#https://pythonbasics.org/pandas-web-scraping/
+ 
+#Install Modules
+
+from multiprocessing.managers import BaseManager
+import lxml 
+import html5lib 
+import bs4
+import pandas as pd
+import openpyxl
+
+#Pandas Web Scraping
+# Webpage url                                                                                                               
+url = 'http://www.bigpumpkins.com/WeighoffResultsGPC.aspx?c=P&y=2021'
+
+# Extract tables
+dfs = pd.read_html(url)
+
+# Get first table                                                                                                           
+df = dfs[0]
+
+# Extract columns                                                                                                           
+df2 = df[['Place','Weight (lbs)', 'Grower Name', 'City', 'State/Prov',	'Country', 'GPC Site',	'Seed (Mother)',	'Pollinator (Father)',	'OTT',	'Est. Weight',	'Pct. Chart']]
+print(df2)
+
+# Write to excel
+df2.to_excel(r'/Users/tbrown/Documents/pumpkin_python2021.xlsx')
+
+#Success!
